349c7650e2796c8628117a9b792571f4f9068973
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-22 12:43:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,8 @@
 from example import create_app
-
-class PrefixMiddleware(object):
-    def __init__(self, app, prefix=''):
-        self.app = app
-        self.prefix = prefix
-
-    def __call__(self, environ, start_response):
-        if environ['PATH_INFO'].startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
-            environ['SCRIPT_NAME'] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url does not belong to the app.".encode()]
+from utils import PrefixMiddleware
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/v1')
     app.run(host="0.0.0.0", port=80)
